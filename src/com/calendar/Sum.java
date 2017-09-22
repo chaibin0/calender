eca6627df9b68b2eeb9920ac9da16779164f226e
chaibin <chaibin@DESKTOP-F24HV8A>
2017-09-22 19:40:19 +0900

    Add sum of two number

---
+++ src/com/calendar/Sum.java
@@ -0,0 +1,22 @@
+package com.calendar;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b;
+		Scanner scanner =new Scanner(System.in);
+		String s1,s2;
+		s1=scanner.next();
+		s2=scanner.next();
+		System.out.println("두 수를 입력해주세요");
+		System.out.println(s1+", "+s2);
+		a=Integer.parseInt(s1);
+		b=Integer.parseInt(s2);
+		System.out.println(a+", "+b);
+		System.out.println("두 수의 합은"+(a+b));
+	}
+
+}
